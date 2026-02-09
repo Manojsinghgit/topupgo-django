437ff51a467171704dc0e594bb55c0662f612c52
RonitPayairo <ronit.s@payairo.com>
2026-02-10 02:05:57 +0530

    changes

--- wallet/urls.py
+++ wallet/urls.py
@@ -15,5 +15,6 @@ urlpatterns = [
     path("wallets/<int:pk>/", WalletDetailAPIView.as_view(), name="wallet-detail"),
     path("transactions/", TransactionListCreateAPIView.as_view(), name="transaction-list-create"),
     path("transactions/<int:pk>/", TransactionDetailAPIView.as_view(), name="transaction-detail"),
-    path("v1/wallets/", WalletMyListAPIView.as_view()),
+    path("v1/wallets/", WalletMyListAPIView.as_view(),name="get-wallet"),
+    path("v1/wallets/<int:wallet_id>/", WalletMyListAPIView.as_view(), name="get-wallet-detail"),
 ]
